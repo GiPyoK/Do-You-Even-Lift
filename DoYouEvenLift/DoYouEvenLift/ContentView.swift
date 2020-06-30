eca4c94f6fcdd40edfaeac084580c39c89a739b8
Gi Pyo Kim <gipyok@gmail.com>
2020-06-29 18:08:02 -0700

    Initial commit

---
+++ DoYouEvenLift/DoYouEvenLift/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DoYouEvenLift
+//
+//  Created by Gi Pyo Kim on 6/29/20.
+//  Copyright © 2020 gipgip. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
